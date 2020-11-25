7f6e1d006cadad080f3e3d3debf55db511e876b0
wahed2102 <wahed.khan@rediffmail.com>
2020-11-25 16:02:37 +0530

    News started

--- src/App.js
+++ src/App.js
@@ -7,23 +7,37 @@ import Photo from "./images/wahed.jpg";
 import { LandingPage } from "./Components/LandingPage";
 import Footer from "./Components/Footer";
 import { NavBar } from "./Components/NavBar";
+import News from "./Components/News/News";
 
 function App() {
   return (
     <div>
       <NavBar></NavBar>
-      <div style={{ width: "1128px", margin: "0px auto" }}>
-        <Profile
-          name="Wahed Ali"
-          alt="Wahed Ali"
-          image={Photo}
-          description="Fellow - Full stack Developer Masai School Full-time"
-          background={Background}
-          connections={33}
-          viewed={16}
-        />
-        {/* <LandingPage></LandingPage>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          width: "1128px",
+          margin: "0px auto",
+          marginTop: "49px",
+        }}
+      >
+        <div>
+          <Profile
+            name="Wahed Ali"
+            alt="Wahed Ali"
+            image={Photo}
+            description="Fellow - Full stack Developer Masai School Full-time"
+            background={Background}
+            connections={33}
+            viewed={16}
+          />
+          {/* <LandingPage></LandingPage>
       <Footer /> */}
+        </div>
+        <div style={{ float: "right" }}>
+          <News />
+        </div>
       </div>
     </div>
   );
---
+++ src/Components/News/News.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./News.module.css";
+
+const News = () => {
+  return <div className={styles.main}></div>;
+};
+
+export default News;
