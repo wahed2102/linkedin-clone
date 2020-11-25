76e6eef9dff03ce673b7a1c69c0da0e6207db023
wahed2102 <wahed.khan@rediffmail.com>
2020-11-25 17:14:35 +0530

    updated

--- src/Components/News/News.jsx
+++ src/Components/News/News.jsx
@@ -2,7 +2,21 @@ import React from "react";
 import styles from "./News.module.css";
 
 const News = () => {
-  return <div className={styles.main}></div>;
+  return (
+    <div className={styles.main}>
+      <h2>LinkedIn News</h2>
+      <ul>
+        <li>
+          <a href="#">
+            <div>
+              <span className={styles.news_bullet}></span>
+              Google is eyeing ShareChat: Report
+            </div>
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default News;
